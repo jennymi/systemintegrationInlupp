c90edbcbc0e5ad9acb6300654ee221c49ee21014
gustafeden <gustafeden@gmail.com>
2019-05-15 15:20:46 +0200

    working graph

--- IoTDeviceMonitor/src/main/webapp/websocket.js
+++ IoTDeviceMonitor/src/main/webapp/websocket.js
@@ -6,7 +6,10 @@
 
 
 var ws;
-
+var chart;
+var list = [];
+var series;
+var interval, visits = 1;
 function connect() {
     //var username = document.getElementById("username").value;
 
@@ -36,12 +39,12 @@ function getList() {
     ws.send(json);
 }
 
-function printList(list){
+function printList(list) {
     var text = "<table ><thead><tr><th>Temperatur </th><th>Luftfuktighet </th><th>Tidsstämpel </th></tr></thead>";
-    for(var i=0; i<10; i++){
-        text += "<tbody><tr><td>"+list[i].temperature+" </td><td>"+list[i].humidity+"</td><td>"+list[i].created+"</td></tr></tbody>";
+    for (var i = 0; i < 10; i++) {
+        text += "<tbody><tr><td>" + list[i].temperature + " </td><td>" + list[i].humidity + "</td><td>" + list[i].created + "</td></tr></tbody>";
     }
-    text +=  "</table>";
+    text += "</table>";
     document.getElementById("printList").innerHTML = text;
 }
 window.onbeforeunload = function () {
@@ -50,16 +53,19 @@ window.onbeforeunload = function () {
 };
 window.onload = function () {
     ws = new WebSocket("ws://localhost:8080/IoTDeviceMonitor/device");
+    loadChart();
     ws.onmessage = function (event) {
         console.log("message arrived");
         var log = document.getElementById("log");
         var message = JSON.parse(event.data);
-        if (message.device == "list") {
+        if (message.device === "list") {
             console.log("loading chart");
             printList(message.table);
-            loadChart(message.table);
         } else {
-
+            //var lastdataItem = series.dataItems.getIndex(series.dataItems.length - 1);
+            chart.addData(
+                    {date: Date.now(), value: message.temperature}
+                    );
             var splittedtemp = message.temperature.split(".");
             var temp = {first: splittedtemp[0], dec: splittedtemp[1]};
             loadTempIcon(temp);
@@ -79,7 +85,7 @@ function loadHumidIcon(humids) {
     var temp = document.getElementById("humidicon");
     temp.innerHTML = ((humids.first !== null) ? humids.first : 0) + "<span id='humiddecimal'>.0</span><strong>&deg;</strong>";
     var tempdec = document.getElementById("humiddecimal");
-    tempdec.innerHTML = "." + ((humids.dec < 1) ? humids.dec : 0);
+    tempdec.innerHTML = "." + ((humids.dec > 1) ? humids.dec : 0);
 }
 function loadTempIcon(temps) {
     var temp = document.getElementById("tempicon");
@@ -88,19 +94,9 @@ function loadTempIcon(temps) {
     tempdec.innerHTML = "." + ((temps.dec > 1) ? temps.dec : 0);
 }
 
-function loadChart(data) {
-    var list = [];
-    for (var x in data) {
-        var t = data[x].created.split(/[- :]/);
-        var d = new Date(Date.UTC(t[0], t[1] - 1, t[2], t[3], t[4], t[5]));
-        list.push({
-            date: d,
-            value: data[x].temperature
-        });
-    }
+function loadChart() {
     am4core.useTheme(am4themes_animated);
-    var chart = am4core.create("chartdiv", am4charts.XYChart);
-    chart.colors.step = 2;
+    chart = am4core.create("chartdiv", am4charts.XYChart);
     chart.hiddenState.properties.opacity = 0;
 
     chart.padding(0, 0, 0, 0);
@@ -108,8 +104,13 @@ function loadChart(data) {
     chart.zoomOutButton.disabled = true;
 
 
+    var data = [];
 
-    chart.data = list;
+    data.push({date: Date.now(), value: 20});
+
+
+    chart.data = data;
+
     var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
     dateAxis.renderer.grid.template.location = 0;
     dateAxis.renderer.minGridDistance = 30;
@@ -141,30 +142,55 @@ function loadChart(data) {
     chart.events.on("datavalidated", function () {
         dateAxis.zoom({start: 1 / 15, end: 1.2}, false, true);
     });
+
     dateAxis.interpolationDuration = 500;
     dateAxis.rangeChangeDuration = 500;
 
-    document.addEventListener("visibilitychange", function () {
-        if (document.hidden) {
-            if (interval) {
-                clearInterval(interval);
-            }
-        } else {
-            startInterval();
-        }
-    }, false);
 
-    var interval;
-    function startInterval() {
-        interval = setInterval(function () {
-            visits =
-                    visits + Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 5);
-            var lastdataItem = series.dataItems.getIndex(series.dataItems.length - 1);
-            chart.addData(
-                    {date: new Date(lastdataItem.dateX.getTime() + 1000), value: visits},
-                    1
-                    );
-        }, 1000);
-    }
+    series.fillOpacity = 1;
+    var gradient = new am4core.LinearGradient();
+    gradient.addColor(chart.colors.getIndex(0), 0.2);
+    gradient.addColor(chart.colors.getIndex(0), 0);
+    series.fill = gradient;
+
+// this makes date axis labels to fade out
+    dateAxis.renderer.labels.template.adapter.add("fillOpacity", function (fillOpacity, target) {
+        var dataItem = target.dataItem;
+        return dataItem.position;
+    });
+
+// need to set this, otherwise fillOpacity is not changed and not set
+
+
+// this makes date axis labels which are at equal minutes to be rotated
+    dateAxis.renderer.labels.template.adapter.add("rotation", function (rotation, target) {
+        var dataItem = target.dataItem;
+        if (dataItem.date && dataItem.date.getTime() === am4core.time.round(new Date(dataItem.date.getTime()), "minute").getTime()) {
+            target.verticalCenter = "middle";
+            target.horizontalCenter = "left";
+            return -90;
+        } else {
+            target.verticalCenter = "bottom";
+            target.horizontalCenter = "middle";
+            return 0;
+        }
+    });
+
+// bullet at the front of the line
+    var bullet = series.createChild(am4charts.CircleBullet);
+    bullet.circle.radius = 5;
+    bullet.fillOpacity = 1;
+    bullet.fill = chart.colors.getIndex(0);
+    bullet.isMeasured = false;
+
+    series.events.on("validated", function () {
+        bullet.moveTo(series.dataItems.last.point);
+        bullet.validatePosition();
+    });
+    dateAxis.events.on("validated", function () {
+        am4core.iter.each(dateAxis.renderer.labels.iterator(), function (label) {
+            label.fillOpacity = label.fillOpacity;
+        });
+    });
 
 }
